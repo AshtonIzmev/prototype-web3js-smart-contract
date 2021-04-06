31287594e9dbdfdd8914c8920adceed58ece377f
Issam <issam.git@issam.ma>
2021-04-07 00:35:50 +0100

    started developping smart contract logic for MED ERC20$

--- assosyndic/js/curieux_storage.js
+++ assosyndic/js/curieux_storage.js
@@ -2,41 +2,6 @@
 // LOCAL STORAGE
 /////////////////////////
 
-var keyLocalStorage = "events_v0.3";
-var emptyRes = '{"r":[]}';
-
-function getStore() {
-    return window.localStorage.getItem(keyLocalStorage) || emptyRes;
-};
-
-function addToStore(assoc, typeStore) {
-    var current = window.localStorage.getItem(keyLocalStorage) || emptyRes;
-    dicur = JSON.parse(current);
-    dicur['r'].push((assoc + '|' + new Date().toISOString() + "|" + typeStore));
-    window.localStorage.setItem(keyLocalStorage, JSON.stringify(dicur));
-};
-
-function addRechercheToStore(assoc, typeStore) {
-    var current = window.localStorage.getItem(keyLocalStorage) || emptyRes;
-    dicur = JSON.parse(current);
-    var ind = dicur['r'].map(x => x.split("|")[0]).indexOf(assoc);
-    if (ind > -1) {
-        return;
-    }
-    dicur['r'].push((assoc + '|' + new Date().toISOString() + "|" + typeStore));
-    window.localStorage.setItem(keyLocalStorage, JSON.stringify(dicur));
-};
-
-function removeFromStore(assoc) {
-    var current = window.localStorage.getItem(keyLocalStorage) || emptyRes;
-    dicur = JSON.parse(current);
-    var ind = dicur['r'].map(x => x.split("|")[0]).indexOf(assoc);
-    if (ind > -1) {
-        dicur['r'].splice(ind, 1);
-    }
-    window.localStorage.setItem(keyLocalStorage, JSON.stringify(dicur));
-};
-
 function addAssociationToStore(assoc) { addToStore(assoc, "association"); };
 function addCooptationToStore(assoc) { addToStore(assoc, "cooptation"); };
 function addBanToStore(assoc) { addToStore(assoc, "ban"); };
--- mederc20/js/med.js
+++ mederc20/js/med.js
@@ -11,12 +11,13 @@ async function main() {
             setBalance(web3, account);
         }
     });
+    loadHistoric();
 };
 
 function setBalance(w3, account) {
     w3.eth.getBalance(account, function (err, balance) {
-        $("#solde").html((w3.utils.fromWei(balance, 'ether')).substring(0, 5) + "ETH");
-        $("#accountaddress").html(account.substring(0, 15)+"...");
+        $("#solde").html((w3.utils.fromWei(balance, 'ether')).substring(0, 5) + "MED");
+        $("#accountaddress").html(account.substring(0, 15) + "...");
         $("#completeaccountaddress").html(account);
     });
 }
@@ -30,7 +31,38 @@ async function refreshEther() {
     });
 };
 
+function addTransferToStore(trsf) { addToStore(trsf, "transfert"); };
 
+function loadHistoric() {
+    var eventList = JSON.parse(getStore())['r'];
+    $("#tbodyevents").html("");
+    eventList.forEach(function (a) {
+        var tuple = a.split("|");
+        if (tuple[2] == "transfert") {
+            var benef_amount = tuple[0].split(';');
+            $("#tbodyevents").append(
+                "<tr><td class='ht'>" + benef_amount[0].substring(0, 10) + "..." +
+                "</td><td>" + benef_amount[1] + "</td><td>" + tuple[1] + "</td></tr>"
+            );
+        }
+    });
+};
 
+function transfer() {
+    var benef = $("#trsfInputBenef").val();
+    var amount = $("#trsfInputAmount").val();
+    addTransferToStore(benef + ";" + amount);
+    alert(benef + amount);
+    loadHistoric();
+}
+
+function deployMED() {
+    var treasureAdd = $("#paramctr1").text();
+    var ratePct = $("#paramctr2").text();
+    var umi = $("#paramctr3").text();
+    alert(treasureAdd + ratePct + umi);
+}
 
 window.addEventListener('load', main);
+
+// web3.eth.sendTransaction({from:"0x981093C7A5AA41b6b5f754c27b3a62adBB34Dd7a", to:"0x132f15ff1af2c9741d10e12ddc141574eecadd25", value:web3.utils.toWei("1", "ether")});
