4f4fb93770a22f8310c26ad6be59f59f8ae808bc
Issam <biossamu@gmail.com>
2021-05-28 23:41:01 +0100

    Cleaning homepage

--- assets/geth/med.month.js
+++ assets/geth/med.month.js
@@ -1,9 +1,8 @@
 
 loadScript("/home/ethereum/tmp/med.abi.js")
 loadScript("/home/ethereum/tmp/med.bytecode.js")
-loadScript("/home/ethereum/tmp/med.deploy.js")
 
-var med = eth.contract(abi).at("0x91EA2D6Ed5F19b877abEC6B4597331E5D81F35d3");
+var med = eth.contract(abi).at("0x01AbA88b557c46dC82D9B6f8367125370AB23c49");
 
 med.incrementMonth({from: eth.coinbase});
 
